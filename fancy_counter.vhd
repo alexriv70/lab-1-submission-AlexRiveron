8980ec9a381a2cb11ed014f67046e5ab47ff0ffa
AlexRiveron <47648183+AlexRiveron@users.noreply.github.com>
2019-02-26 22:11:55 -0500

    Update fancy_counter.vhd

--- fancy_counter.vhd
+++ fancy_counter.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -36,16 +37,55 @@ entity fancy_counter is
            clk_en : in STD_LOGIC;
            dir : in STD_LOGIC;
            en : in STD_LOGIC;
-           Id : in STD_LOGIC;
+           ld : in STD_LOGIC;
            rst : in STD_LOGIC;
            updn : in STD_LOGIC;
            val : in STD_LOGIC_VECTOR (3 downto 0);
-           cnt : out STD_LOGIC_VECTOR (3 downto 0));
+           cnt : inout STD_LOGIC_VECTOR (3 downto 0));
 end fancy_counter;
 
 architecture Behavioral of fancy_counter is
 
+signal temp: std_logic := '0';
+
 begin
-
-
+    process(clk)
+    begin
+        if(rising_edge(clk)) then
+            if (en = '1') then
+                if (rst = '0') then
+                    if (clk_en = '1') then
+                        if (updn ='1') then
+                            temp <= dir;
+                        end if;
+                        if (ld = '1') then
+                            cnt <= val;
+                        else if (temp = '1') then
+                            if (cnt = val) then
+                                cnt <= "0000";
+                            else 
+                                cnt <= STD_LOGIC_VECTOR (unsigned(cnt) + 1);
+                            end if;
+                        else if( temp = '0') then
+                            if (cnt = "0000") then
+                                cnt <= val;
+                            else 
+                                cnt <= STD_LOGIC_VECTOR (unsigned(cnt) - 1);
+                            end if;
+                        end if;
+                        end if;
+                        end if;
+                    end if;
+                else if (rst = '1') then
+                    cnt <= "0000";
+                    if (clk_en = '1') then
+                        if (updn = '1') then
+                            temp <= dir;
+                        end if;
+                    end if;
+                end if;
+                end if;
+            end if;
+        end if;
+    end process;
 end Behavioral;
